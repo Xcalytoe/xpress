6bb8b6e83f9159acdca84381f3d10f118266f38c
Ezejiofor Cosmas <ezejioforcosmas@gmail.com>
2023-08-22 06:06:34 +0100

    feat: make verifiers count dynamic

--- src/pages/dashboard/VerifiersPage.tsx
+++ src/pages/dashboard/VerifiersPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from '../../__layouts/Dashboard';
 import { StyledFlex } from '../../components/__styles/ui-block.style';
 import {
@@ -7,8 +7,19 @@ import {
 } from '../../components/__styles/global.style';
 import { styled } from 'styled-components';
 import VerifiersView from '../../components/VerifiersView';
+import { Dispatch, RootState } from '../../redux/store';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Verifiers = () => {
+  const dispatch = useDispatch<Dispatch>();
+  const { verifiers } = useSelector((root: RootState) => root.generalModel);
+  const verifiersCount = verifiers ? verifiers.length : '0';
+
+  //   Fetch verifiers if it does not exixt
+  useEffect(() => {
+    !verifiers && dispatch.generalModel.getVerifiers();
+  }, [dispatch.generalModel, verifiers]);
+
   const title = (
     <StyledFlex $cg="8px" $align="center">
       <HeadingText
@@ -25,7 +36,7 @@ const Verifiers = () => {
         $fw="400"
         $color="var(--background-primary)"
       >
-        11
+        {verifiersCount}
       </StyledCount>
     </StyledFlex>
   );
@@ -44,6 +55,6 @@ const StyledCount = styled(ParagraphText)`
   align-content: center;
   justify-content: center;
   border-radius: 50%;
-  height: 24px;
-  width: 24px;
+  height: 28px;
+  width: 28px;
 `;
